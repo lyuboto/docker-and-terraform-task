f5e09407c33b91c1ff1df89205ee6e52b7e74bd8
unknown <lubocrita@gmail.com>
2020-09-03 10:30:33 +0300

    updating vars

--- terraformtask/variables.tf
+++ terraformtask/variables.tf
@@ -4,12 +4,19 @@ variable "docker_container_name" {
 variable "docker_volume_name" {
   type = string
 }
-variable "docker_network_name" {
-  type = string
-}
-variable "ext_port" {}
+#variable "docker_network_name" {
+#  type = string
+#}
+#varibale "ext_port" {}
 variable "docker_host" {}
 variable "docker_image" {}
-variable "docker_path" {}
+varibale "docker_path" {}
 variable "int_port" {}
 variable "restart_type" {}
+
+variable "terratask" {
+  type = object ({
+    docker_network_name = string
+	ext_port = int
+  })
+}
