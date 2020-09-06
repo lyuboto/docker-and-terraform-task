78cde9f3e7856ad340d66e236c2de0d4add358d3
root <lubocrita@gmail.com>
2020-09-06 13:36:09 +0300

    fixed formatting

---
+++ terraformtask/build-image.sh
@@ -0,0 +1,2 @@
+cd /home/lyubo/Desktop/tf_test_repo/docker-and-terraform-task/dockertask
+docker build -t nx .
--- terraformtask/variables.tf
+++ terraformtask/variables.tf
@@ -1,13 +1,13 @@
 variable "my_env" {
-  type = object ({
-    docker_container_name = string
-	docker_volume_name = string
-	docker_network_name = string
-	docker_host = string
-	docker_image = string
-	docker_path = string
-	restart_type = string
-	int_port = number
-	ext_port = number
-  })
+	type = object ({
+		docker_container_name = string
+		docker_volume_name = string
+		docker_network_name = string
+		docker_host = string
+		docker_image = string
+		docker_path = string
+		restart_type = string
+		int_port = number
+		ext_port = number
+	})
 }
